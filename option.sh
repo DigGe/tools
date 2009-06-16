137e397ec06a4d3161a986e4253a9426569dc9c5
Dig Ge <Dig.Ge.CN@Gmail.com>
2009-06-16 10:18:29 +0800

    update files

--- option.sh
+++ option.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 DIR="/sys/class/scsi_generic"
 for file in `ls $DIR`
 do
 	if [ "ZCOPTION" == $(cat $DIR/$file/device/vendor) ] && [[ "HSDPA Modem     " == "$(cat $DIR/$file/device/model)" ]]
 	then
 		echo $file;
-		rezero /dev/$file
+		sudo rezero /dev/$file
 	fi
 done
 
---
+++ remove_old_package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /var/cache/apt/archives/
+
+for file in `ls | sort | cut -d_ -f1`
+do
+	count=`ls $file\_*| wc -l`
+	#echo $file $count
+	if [[ $count -ne 1 ]]
+	then
+		echo $file $count
+		count=`expr $count - 1`
+		sudo mv `ls "$file_*"| head -n $count` ./tobedelete/
+	fi
+done
+
+cd -
+
