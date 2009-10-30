8d9bc9bb21a2291f475c65925cc6e079bc78d305
Dig Ge <Dig.Ge.CN@Gmail.com>
2009-10-30 13:51:42 +0800

    delete uninstalled package

--- remove_old_package.sh
+++ remove_old_package.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+USER=`env | grep LOGNAME | cut -d\= -f2`
+if [ -z "$USER" ] || [ "$USER" != "root" ]
+then
+	echo "you must run this script as root"
+	exit 1
+fi
+
+PKG_FILE=/tmp/pkg.list
 cd /var/cache/apt/archives/
+dpkg -l > $PKG_FILE
 
 for file in `ls | sort | cut -d_ -f1`
 do
+	grep "\ $file\ " $PKG_FILE >&/dev/null
+	if [ $? -ne 0 ]
+	then
+		echo "$file not installed"
+		mv "$file"_* ./tobedelete/
+	fi
 	count=`ls "$file"_*| wc -l`
-	#echo $file $count
-	if [[ $count -ne 1 ]]
+	if [ $count -ne 1 ]
 	then
 		echo $file $count
 		count=`expr $count - 1`
-		sudo mv `ls "$file"_*| head -n $count` ./tobedelete/
+		mv `ls "$file"_*| head -n $count` ./tobedelete/
 	fi
 done
 
